5dc5805fe14067431571f67f4cc6bddf0e18cc8f
Nguyễn Thị Xuân Hồng <38204046+nhjp23@users.noreply.github.com>
2018-04-12 11:28:35 +0700

    Add files via upload

--- Supa IDS App/main.py
+++ Supa IDS App/main.py
@@ -1,20 +1,23 @@
+from Tkinter import *
 import Tkinter as tk            
 import tkFont  as tkfont
 import tkMessageBox as tkmb
-import datetime
-now = datetime.datetime.now()
-import re
-from Tkinter import *
-import send_email
+
+import time
 import socket
-import runsnort as run
-from threading import Event
-from threading import Thread
+import re
 import os
 import Queue
+
+from threading import Event
+from threading import Thread
 from scapy.all import *
-import time
-#import sql
+
+
+import send_email
+import getIface as g
+import exportFiles as e
+import runsnort as run
 
 class SampleApp(tk.Tk):
 	def __init__(self, *args, **kwargs):
@@ -30,7 +33,7 @@ class SampleApp(tk.Tk):
 	        container.grid_columnconfigure(0, weight=1)
 	
 	        self.frames = {}
-	        for F in (HomePage, Capture, Graph, About, Register):
+	        for F in (HomePage, Register, ChooseIface, Capture, Graph, About):
 	            page_name = F.__name__
 	            frame = F(parent=container, controller=self)
 	            self.frames[page_name] = frame
@@ -63,8 +66,8 @@ class HomePage(tk.Frame):
 		spacer1 = Label(self, text="\n", height=1)
 		spacer1.pack()	
 
-		btnCapture = tk.Button(self, text="Capture", command=lambda: controller.show_frame("Capture"), width=10, font=controller.button_font)
-		btnCapture.pack()
+		btnChooseIface = tk.Button(self, text="Capture", command=lambda: controller.show_frame("ChooseIface"), width=10, font=controller.button_font)
+		btnChooseIface.pack()
 
 		spacer2 = Label(self, text="\n", height=1)
 		spacer2.pack()	
@@ -83,52 +86,25 @@ class Register(tk.Frame):
 	def __init__(self, parent, controller):
 
 		def btnSaveClicked():
-			file = open('user_info.txt', 'r') 
-			info = file.read() 
-			try:
-				uname = info.split(',')[1]
+			if not entry1.get():
+				tkmb.showerror("Error","Please enter your name!")				
+			else:
 				if not entry2.get():
-						tkmb.showerror("Error","Please enter your email!")					
+					tkmb.showerror("Error","Please enter your email!")					
 				else:
 					if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", entry2.get()):
-							tkmb.showerror("Error","Your email is not valid!")
+						tkmb.showerror("Error","Your email is not valid!")
 					else:
+						username = entry1.get()
 						mail = entry2.get()
+						print username
 						print mail
-						ip = info.split(',')[0]
+
 						file = open('user_info.txt','w') 
-						file.write(ip+','+uname+','+mail) 
+						file.write(username+','+mail) 
 						file.close()
-						tkmb.showinfo("Success","Email saved!")	
-			except:
-				if not entry1.get():
-					tkmb.showerror("Error","Please enter your name!")				
-				else:
-					if not entry2.get():
-						tkmb.showerror("Error","Please enter your email!")					
-					else:
-						if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", entry2.get()):
-							tkmb.showerror("Error","Your email is not valid!")
-						else:
-							username = entry1.get()
-							mail = entry2.get()
-							ip = get_ip_address()
-							print ip
-							print username
-							print mail
-
-							file = open('user_info.txt','w') 
-							file.write(ip+','+username+','+mail) 
-							file.close()
-							tkmb.showinfo("Success","Success")		
-							print now.strftime("%Y-%m-%d")
+						tkmb.showinfo("Success","Success")	
 				
-
-		def get_ip_address():
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			s.connect(("8.8.8.8", 80))
-			return s.getsockname()[0]
-
 
 		#def btnSendClicked():
 			
@@ -141,19 +117,9 @@ class Register(tk.Frame):
 		frame1.pack(fill=X)
 		lbl1 = Label(frame1, text="Name", width=6, font=controller.label_font)
 		lbl1.pack(side=LEFT, padx=5, pady=5)
-  
-		file = open('user_info.txt', 'r') 
-		info = file.read() 
-		
-		uname = info.split(',')[1]
-		if not uname:
-			entry1 = Entry(frame1, font=18)
-			entry1.pack(fill=X, padx=15, expand=True)
-		else:			
-			entry1 = Entry(frame1, font=18)
-			entry1.pack(fill=X, padx=15, expand=True)
-			entry1.insert(END, uname)
-			entry1.config(state="disabled")
+  		
+		entry1 = Entry(frame1, font=18)
+		entry1.pack(fill=X, padx=15, expand=True)
 
 		frame2 = Frame(self)
 		frame2.pack(fill=X)
@@ -172,9 +138,39 @@ class Register(tk.Frame):
 		lb5 = Label(self, text="\n\n")
 		lb5.pack()	
 		
-		button = tk.Button(self, text="Go back to Home page", command=lambda: controller.show_frame("HomePage"), font=controller.button_font)
+		button = tk.Button(self, text="Home page", command=lambda: controller.show_frame("HomePage"), font=controller.button_font)
 		button.pack()
+
+
+class ChooseIface(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		self.controller = controller
+
+		label = tk.Label(self, text="Choose an interface: ", font=25)
+		label.pack(anchor=W, pady=10)
 		
+		global iface
+		ifaceList = g.all_interfaces()
+
+		def showChoice():
+			global iface
+			iface = str(var.get()).split(' ')[0]
+			print iface
+			btnCapture.config(state='normal')
+		var = StringVar()
+		for i in ifaceList:
+			rd = tk.Radiobutton(self, text = i[0] + ": " + i[1], variable = var, value = i, command=showChoice, font=15)
+			rd.pack(anchor=W)
+		
+		btnCapture = tk.Button(self, text="Capture", command=lambda: controller.show_frame("Capture"), font=controller.button_font)
+		btnCapture.pack(anchor=W, pady=20, padx=10)
+		btnCapture.config(state='disabled')
+		
+		button = tk.Button(self, text="Home page", command=lambda: controller.show_frame("HomePage"), font=controller.button_font)
+		button.pack()
+
+
 class Capture(tk.Frame):
 
 	def __init__(self, parent, controller):
@@ -203,14 +199,14 @@ class Capture(tk.Frame):
 		def walkerLoop():
 			global quit_walking
 			while not quit_walking.is_set():
-				cap = sniff(count=100,timeout=20,iface='ens33')
+				cap = sniff(count=100,timeout=20,iface=iface)
 				q.put(cap)
 						
 
 			quit_walking = Event()
 
 		def checkPcap():
-			import exportFiles as e
+			
 			pcapfile = 'sniff.pcap'
 			global quit
 			while not quit.is_set():
@@ -276,25 +272,24 @@ class Capture(tk.Frame):
 
 			global total
 			total += int(file[0])
+			lbtotal.config(text=total)
 			print total
 					
 			global tcp
 			tcp += int(file[1])
+			lbtcp.config(text=tcp)
 			print tcp
 			
 			global udp
 			udp += int(file[2])
+			lbudp.config(text=udp)
 			print udp
 			
 			global icmp
 			icmp += int(file[3])
+			lbicmp.config(text=icmp)
 			print icmp
-			
-		def runBro():
-			import exportFiles as e
-			e.bro()
-			e.sort()
-			e.exportFile()
+
 
 		tk.Frame.__init__(self, parent)
 		self.controller = controller
@@ -309,33 +304,35 @@ class Capture(tk.Frame):
 		self.rowconfigure(2, pad=10, weight=1)
 		self.rowconfigure(3, pad=10, weight=1)
 		self.rowconfigure(4, pad=10, weight=1)
-		self.rowconfigure(5, pad=0, weight=1)
-		self.rowconfigure(6, pad=0, weight=1)
+		self.rowconfigure(5, pad=10, weight=1)
+		self.rowconfigure(6, pad=10, weight=1)
+		self.rowconfigure(7, pad=10, weight=1)
 
 
 		label = tk.Label(self, text="Capture", font=controller.title_font)
 		label.grid(row=0, columnspan=4, sticky=NSEW)
 		
-		#btnHome = tk.Button(self, text="Go back to Home page", command=lambda: controller.show_frame("HomePage"), font=controller.button_font)
-		#btnHome.grid(row=6, columnspan=4)
+		btnHome = tk.Button(self, text="Home page", command=lambda: controller.show_frame("HomePage"), font=controller.button_font)
+		btnHome.grid(row=7, columnspan=4)
 
 
 		v = tk.IntVar()
 
-		btnStartWithSnort = tk.Button(self, text="Start", command=btnStartWithSnortClicked, width=15, font=controller.button_font, fg='green')
+
+		btnStartWithSnort = tk.Button(self, text="Start (recommended)", command=btnStartWithSnortClicked, width=17, font=controller.button_font, fg='green')
 		btnStartWithSnort.grid(row=1, column=0)
 		btnStartWithSnort.config(font='Arial 14 bold')
 		
-		btnStartWithoutSnort = tk.Button(self, text="Start Without Snort", width=15, command=btnStartWithoutSnortClicked, font=controller.button_font, fg='green')
+		btnStartWithoutSnort = tk.Button(self, text="Start Without Snort", width=17, command=btnStartWithoutSnortClicked, font=controller.button_font, fg='green')
 		btnStartWithoutSnort.grid(row=1, column=1)
 		btnStartWithoutSnort.config(font='Arial 14 bold')
 
-		btnStop = tk.Button(self, text="Stop", command=btnStopClicked, width=15, font=controller.button_font, fg='red')
+		btnStop = tk.Button(self, text="Stop", command=btnStopClicked, width=17, font=controller.button_font, fg='red')
 		btnStop.grid(row=1, column=3)
 		btnStop.config(state='disabled', font='Arial 14 bold')
 
-		btnCheckFile = tk.Button(self, text="count", command=init, width=15, font=controller.button_font)
-		btnCheckFile.grid(row=6, columnspan=4)		
+#		btnCheckFile = tk.Button(self, text="count", command=init, width=15, font=controller.button_font)
+#		btnCheckFile.grid(row=6, columnspan=4)		
 
 #		label = tk.Label(self, text="\n", font=controller.title_font)
 #		label.grid(row=2, columnspan=4, sticky=NSEW)
@@ -349,17 +346,17 @@ class Capture(tk.Frame):
 		totaltitle = tk.Label(self, text="TOTAL", font=15, fg='blue')
 		totaltitle.grid(row=3, column=3)
 
-		tcp = tk.Label(self, text="0", font=15)
-		tcp.grid(row=4, column=0)
-		udp = tk.Label(self, text="0", font=15)
-		udp.grid(row=4, column=1)
-		icmp = tk.Label(self, text="0", font=15)
-		icmp.grid(row=4, column=2)
-		total = tk.Label(self, text="0", font=15, fg='blue')
-		total.grid(row=4, column=3) 		
+		lbtcp = tk.Label(self, text="0", font=15)
+		lbtcp.grid(row=4, column=0)
+		lbudp = tk.Label(self, text="0", font=15)
+		lbudp.grid(row=4, column=1)
+		lbicmp = tk.Label(self, text="0", font=15)
+		lbicmp.grid(row=4, column=2)
+		lbtotal = tk.Label(self, text="0", font=15, fg='blue')
+		lbtotal.grid(row=4, column=3) 		
 		
 		label = tk.Label(self, text="\n\n\n\n\n", font=controller.title_font)
-		label.grid(row=5, columnspan=4, sticky=NSEW)
+		label.grid(row=6, columnspan=4, sticky=NSEW)
 
 class Graph(tk.Frame):
 
@@ -368,7 +365,7 @@ class Graph(tk.Frame):
 	        self.controller = controller
 	        label = tk.Label(self, text="Graph", font=controller.title_font)
 	        label.pack(side="top", fill="x", pady=10)
-	        button = tk.Button(self, text="Go back to Home page",
+	        button = tk.Button(self, text="Home page",
 	                           command=lambda: controller.show_frame("HomePage"), font=controller.button_font)
 	        button.pack()
 
@@ -384,7 +381,7 @@ class About(tk.Frame):
 		lbInfo.config(justify=LEFT)
 		lbInfo.pack()
 
-	        button = tk.Button(self, text="Go back to Home page",
+	        button = tk.Button(self, text="Home page",
 	                           command=lambda: controller.show_frame("HomePage"), font=controller.button_font)
 	        button.pack()
 		
@@ -392,7 +389,7 @@ class About(tk.Frame):
 if __name__ == "__main__":
 
 	app = SampleApp()
-	app.geometry("750x500")
+	app.geometry("800x500")
 	app.title("Supa IDS")
 		
 	def on_closing():
